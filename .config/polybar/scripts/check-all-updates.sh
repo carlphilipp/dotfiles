042eac37130dd465ff9e21890ec57142c093e9be
Carl-Philipp Harmant <cp.harmant@gmail.com>
2019-03-10 20:59:25 -0500

    Improve pacman update

--- .config/polybar/scripts/check-all-updates.sh
+++ .config/polybar/scripts/check-all-updates.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 #source https://github.com/x70b1/polybar-scripts
+set -e
 
-
-if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+if ! updates_arch=$(checkupdates 2> /dev/null | wc -l); then
     updates_arch=0
 fi
 
-# if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
-if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+if ! updates_aur=$(yay -Pu | wc -l); then
     updates_aur=0
 fi
 
--- .config/polybar/scripts/pacman-updates.sh
+++ .config/polybar/scripts/pacman-updates.sh
@@ -6,9 +6,9 @@
 isConnected=$(/bin/sh ~/.config/polybar/scripts/connect.sh)
 if [ "$isConnected" == "1" ]
 then
-    update_manjaro=$(checkupdates | wc -l)
-    updates_aur=$(yay -Pn)
-    updates=$(("$update_manjaro" + "$updates_aur")) 
+    update_manjaro=$(checkupdates 2> /dev/null | wc -l)
+    updates_aur=$(yay -Pu 2> /dev/null | wc -l)
+    updates=$(("$update_manjaro" + "$updates_aur"))
     if [ "$updates" != "0" ]
     then
         echo $updates
