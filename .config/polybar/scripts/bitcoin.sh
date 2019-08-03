664a9cda3e1587da6cd8ecf952dc5e7bd40e3955
Carl-Philipp Harmant <cp.harmant@gmail.com>
2019-08-02 20:23:58 -0500

    Update

--- .config/polybar/scripts/bitcoin.sh
+++ .config/polybar/scripts/bitcoin.sh
@@ -3,7 +3,7 @@
 IS_CONNECTED=$(/bin/sh ~/.config/polybar/scripts/connect.sh)
 if [ "$IS_CONNECTED" == "1" ]
 then
-    RESPONSE=$(curl -s https://min-api.cryptocompare.com/data/pricemultifull\?fsyms\=BTC\&tsyms\=USD)
+    RESPONSE=$(curl -s https://min-api.cryptocompare.com/data/pricemultifull\?fsyms\=BTC\&tsyms\=USD\&api_key=b13f76b096bc9b7e7eac7d83f7e28fec56463194b875fa3d377e26d4db94c078)
     VALUE=$(echo $RESPONSE | json RAW.BTC.USD.PRICE | sed 's/ //g')
     PERCENTAGE=$(echo $RESPONSE | json DISPLAY.BTC.USD.CHANGEPCT24HOUR)
 
--- .config/polybar/scripts/eth.sh
+++ .config/polybar/scripts/eth.sh
@@ -3,10 +3,10 @@
 IS_CONNECTED=$(/bin/sh ~/.config/polybar/scripts/connect.sh)
 if [ "$IS_CONNECTED" == "1" ]
 then
-    RESPONSE=$(curl -s https://min-api.cryptocompare.com/data/pricemultifull\?fsyms\=ETH\&tsyms\=USD)
+    RESPONSE=$(curl -s https://min-api.cryptocompare.com/data/pricemultifull\?fsyms\=ETH\&tsyms\=USD\&api_key=b13f76b096bc9b7e7eac7d83f7e28fec56463194b875fa3d377e26d4db94c078)
     VALUE=$(echo $RESPONSE | json RAW.ETH.USD.PRICE | sed 's/ //g')
     PERCENTAGE=$(echo $RESPONSE | json DISPLAY.ETH.USD.CHANGEPCT24HOUR)
-    
+
     if [ $(echo $PERCENTAGE'>'0 | bc -l) == 1 ] ; then
          PERCENTAGE_DISPLAY=+$PERCENTAGE%
 	else
@@ -16,4 +16,4 @@ then
     echo \$$VALUE $PERCENTAGE_DISPLAY
 else
     echo ?
-fi    
+fi
